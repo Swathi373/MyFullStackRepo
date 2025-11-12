5d722388ae750d770a420d73ac6bdb556e6a68bd
dhara <dhara@172.16.0.123>
2025-11-12 12:26:30 +0530

    Basic Code Structure is initiated

---
+++ GitProj01/src/main/java/com/nit/demo/ArithmeticOperations.java
@@ -0,0 +1,9 @@
+package com.nit.demo;
+
+public class ArithmeticOperations 
+{
+    public int sumOfTwoNumbers(int a,int b)
+    {
+    	return a+b;
+    }
+}
---
+++ GitProj01/src/main/java/com/nit/main/App.java
@@ -0,0 +1,13 @@
+package com.nit.main;
+
+import com.nit.demo.ArithmeticOperations;
+
+public class App
+{
+    public static void main(String[] args) 
+    {
+    	ArithmeticOperations aop = new ArithmeticOperations();
+    	System.out.println("sum of two numbers is: "+aop.sumOfTwoNumbers(10, 20));
+//      System.out.println("Hello World!");
+    }
+}
