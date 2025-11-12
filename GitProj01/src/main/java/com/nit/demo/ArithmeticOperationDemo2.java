1e50fb28f3037de1f92d568c80fef1236b804765
dhara <dhara@172.16.0.123>
2025-11-12 12:44:39 +0530

    Code updated with product logic

---
+++ GitProj01/src/main/java/com/nit/demo/ArithmeticOperationDemo2.java
@@ -0,0 +1,9 @@
+package com.nit.demo;
+
+public class ArithmeticOperationDemo2 
+{
+    public int productOfTwoNum(int a, int b)
+    {
+    	return a*b;
+    }
+}
--- GitProj01/src/main/java/com/nit/main/App.java
+++ GitProj01/src/main/java/com/nit/main/App.java
@@ -1,5 +1,6 @@
 package com.nit.main;
 
+import com.nit.demo.ArithmeticOperationDemo2;
 import com.nit.demo.ArithmeticOperations;
 
 public class App
@@ -9,5 +10,7 @@ public class App
     	ArithmeticOperations aop = new ArithmeticOperations();
     	System.out.println("sum of two numbers is: "+aop.sumOfTwoNumbers(10, 20));
 //      System.out.println("Hello World!");
+    	ArithmeticOperationDemo2 aop2 = new ArithmeticOperationDemo2();
+    	System.out.println("Product of two numbers: "+aop2.productOfTwoNum(10, 20));
     }
 }
