f59167ad2fb7a1f2379d1efa69f7574ee92b54a0
dhara <dhara@172.16.0.223>
2025-11-17 11:35:35 +0530

    code update2

--- GitProj02/src/main/java/com/nit/main/App.java
+++ GitProj02/src/main/java/com/nit/main/App.java
@@ -10,6 +10,6 @@ public class App
         System.out.println(upi.paymentProcess(37865784876L, 25000.0));
         System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         System.out.println(upi.paymentThroughUPIid("ybl@134", 30000.0));
-        System.out.println(upi.paymentThroughAccount(99988877L, "poorna", 2000.0));
+//        System.out.println(upi.paymentThroughAccount(99988877L, "poorna", 2000.0));
     }
 }
