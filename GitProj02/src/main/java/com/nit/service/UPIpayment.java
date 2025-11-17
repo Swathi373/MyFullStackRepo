bbad5f7f1292c99af9990316acc674e476b2c8d6
dhara <dhara@172.16.0.223>
2025-11-17 11:26:45 +0530

    code change2

--- GitProj02/src/main/java/com/nit/service/UPIpayment.java
+++ GitProj02/src/main/java/com/nit/service/UPIpayment.java
@@ -11,4 +11,9 @@ public class UPIpayment
     {
     	return amount+" amount transfered from up id: "+upid;
     }
+    
+    public String paymentViaBank(String name,long accno, double amount)
+    {
+    	return "Account holders name: "+name+", Account number: "+accno+", amount: "+amount;
+    }
 }
